f6c659c702ff7c4316789c0fd8b32c1c754d79be
Sören Rohweder <s.rohweder@blage.net>
2012-11-24 15:49:22 +0100

    [TASK] travis tweaks

--- travisci/bin/ci/install_dependencies.sh
+++ travisci/bin/ci/install_dependencies.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-pwd
+#!/usr/bin/bash
+
 AMQ_VERSION="5.7.0"
 wget http://mirror.synyx.de/apache/activemq/apache-activemq/${AMQ_VERSION}/apache-activemq-${AMQ_VERSION}-bin.tar.gz
 tar -xzf apache-activemq-${AMQ_VERSION}-bin.tar.gz
 
-cp travisci/conf/amq/activemq.xml travisci/bin/ci/apache-activemq-${AMQ_VERSION}/conf/activemq.xml
+cp travisci/conf/amq/activemq.xml apache-activemq-${AMQ_VERSION}/conf/activemq.xml
 
 apache-activemq-${AMQ_VERSION}/bin/linux-x86-32/activemq start
+sleep 10
 
